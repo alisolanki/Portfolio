769cd7bbb2b64bc218d9f66e16049a3a1292efc7
Ali Solanki <55312000+alisolanki@users.noreply.github.com>
2021-05-06 12:28:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -218,49 +218,53 @@
 
 	/* local validation */
 	$('#contactForm').validate({
+		sLoader.fadeOut(); 
+	       $('#message-warning').hide();
+	       $('#contactForm').fadeOut();
+	       $('#message-success').fadeIn();
 
-		/* submit via ajax */
-		submitHandler: function(form) {
+// 		/* submit via ajax */
+// 		submitHandler: function(form) {
 
-			var sLoader = $('#submit-loader');
+// 			var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+// 			$.ajax({      	
 
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
+// 		      type: "POST",
+// 		      url: "inc/sendEmail.php",
+// 		      data: $(form).serialize(),
+// 		      beforeSend: function() { 
 
-		      	sLoader.fadeIn(); 
+// 		      	sLoader.fadeIn(); 
 
-		      },
-		      success: function(msg) {
+// 		      },
+// 		      success: function(msg) {
 
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
+// 	            // Message was sent
+// 	            if (msg == 'OK') {
+// 	            	sLoader.fadeOut(); 
+// 	               $('#message-warning').hide();
+// 	               $('#contactForm').fadeOut();
+// 	               $('#message-success').fadeIn();   
+// 	            }
+// 	            // There was an error
+// 	            else {
+// 	            	sLoader.fadeOut(); 
+// 	               $('#message-warning').html(msg);
+// 		            $('#message-warning').fadeIn();
+// 	            }
 
-		      },
-		      error: function() {
+// 		      },
+// 		      error: function() {
 
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
+// 		      	sLoader.fadeOut(); 
+// 		      	$('#message-warning').html("Something went wrong. Please try again.");
+// 		         $('#message-warning').fadeIn();
 
-		      }
+// 		      }
 
-	      });     		
-  		}
+// 	      });     		
+//   		}
 
 	});
 
